fee141caca492bf09fab6a1ba614474fad19f31f
Saurabh Singh <saurabhworkaholic1111@gmail.com>
2026-09-09 12:17:02 +0530

    Time: 0 ms (100%), Space: 43 MB (5.72%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words=s.split(" ");
+        if(words.length!=pattern.length()){
+            return false;
+        }
+
+        HashMap<Character,String> map=new HashMap<>();
+        HashSet<String> set=new HashSet<>();
+
+        for(int i=0;i<pattern.length();i++){
+            char c=pattern.charAt(i);
+            String word=words[i];
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(word)){
+                    return false;
+                }
+
+            }else{
+                if(set.contains(word)){
+                    return false;
+                }
+                map.put(c,word);
+                set.add(word);
+
+            }
+        }
+
+        return true;
+
+        
+    }
+}
